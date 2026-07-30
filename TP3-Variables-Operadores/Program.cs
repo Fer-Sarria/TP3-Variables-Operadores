7319c155f77987833c578c97d80755577fead07d
Fer-Sarria <fer.cabj.007@gmail.com>
2026-07-29 22:10:51 -0300

    Resolución Ejercicio 1

--- TP3-Variables-Operadores/Program.cs
+++ TP3-Variables-Operadores/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace TP3_Variables_Operadores
+﻿// 1. Operador de incremento de postfijo. ¿Qué sucede con i++?
+// int i = 3;
+// Console.WriteLine(i);
+// Console.WriteLine(i++);
+// Console.WriteLine(i);
+
+using System;
+namespace TP3_Variables_Operadores
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ejercicio1();
+        }
+
+        static void Ejercicio1()
+        {
+            Console.WriteLine("\nOperador de incremento de postfijo (i++)");
+
+            int i = 3;
+            Console.WriteLine(i);
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
         }
     }
 }
